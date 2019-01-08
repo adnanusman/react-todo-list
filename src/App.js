196b52449aacac70981aed7a7238bd5af46cfc98
Adnan Usman <adnantu@hotmail.com>
2019-01-07 22:13:10 -0800

    set up react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <input type="checkbox" id="checkbox1" name="checkbox1" />
+      <label htmlFor="checkbox1">Checkbox 1</label>
+      <br />
+      <input type="checkbox" id="checkbox2" name="checkbox2" />
+      <label htmlFor="checkbox2">Checkbox 2</label>
+      <br />
+      <input type="checkbox" id="checkbox3" name="checkbox3" />
+      <label htmlFor="checkbox3">Checkbox 3</label>
+    </div>
+  );
 }
 
 export default App;
