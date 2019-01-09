2ca87d591c79218d6c64242dfba38ad4bb2aadf4
Adnan Usman <adnantu@hotmail.com>
2019-01-08 19:54:14 -0800

    add todos to state and fix todo list alignment

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import todosData from './data/todosData';
 import TodoItem from './components/TodoItem';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    }
+    
+  };
+
   render() {
-    const todoItemComponents = todosData.map(todo => {
+    const todoItemComponents = this.state.todos.map(todo => {
       return (
         <TodoItem key={todo.id} todo={todo} />
-      )
-    })
+      );
+    });
 
     return (
       <div className="todo-list">
         {todoItemComponents}
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
